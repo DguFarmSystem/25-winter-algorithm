13804692acdaf056062badc12ce8e8569bde9c0b
jooh__0826 <110390562+parkjoohyung0826@users.noreply.github.com>
2025-03-18 13:29:32 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 1 1 2 3 5 8 13 21 34 55
+
+vector<long> dp(1002, -1);
+vector<long> v = {1, 1};
+
+long cal(long num) {
+    // cout << dp[num] << '\n';
+    if(num <= 1) {
+        return dp[num];
+    }
+    if(dp[num] != -1) return dp[num];
+    return dp[num] = (cal(num - 1) % 10007) + 2*(cal(num - 2) % 10007);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    for(int i = 0; i < v.size(); i++) {
+        dp[i] = v[i];
+    }
+    
+    int N;
+    cin >> N;
+    cout << cal(N) % 10007;
+    
+    return 0;
+}
